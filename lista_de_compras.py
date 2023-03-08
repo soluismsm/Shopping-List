75d1a0df33b7abfe8164f966b5434a143a927866
Luis Miguel <vluismw@gmail.com>
2023-03-08 09:59:27 -0300

    EN

--- lista_de_compras.py
+++ lista_de_compras.py
@@ -1,9 +1,9 @@
 from os import system
 
-MENSAGEM_DE_ERRO = "Opção inválida. tente novamente."
+ERROR_MESSAGE = "Opção inválida. tente novamente."
 
 
-def tela_inicial():
+def get_option():
     print("Selecione uma opção")
     print("1 - Inserir item na lista")
     print("2 - Apagar tem da lista")
@@ -11,76 +11,76 @@ def tela_inicial():
     print("4 - Sair")
     while True:
         try:
-            escolha = int(input("Escolha (1|2|3|4): "))
-            if escolha == 1:
+            choice = int(input("Escolha (1|2|3|4): "))
+            if choice == 1:
                 return 1
-            elif escolha == 2:
+            elif choice == 2:
                 return 2
-            elif escolha == 3:
+            elif choice == 3:
                 return 3
-            elif escolha == 4:
+            elif choice == 4:
                 return 4
             else:
-                raise ValueError(MENSAGEM_DE_ERRO)
+                raise ValueError(ERROR_MESSAGE)
         except ValueError:
-            print(MENSAGEM_DE_ERRO)
+            print(ERROR_MESSAGE)
 
 
-def mostrar_lista_de_compras():
-    if len(lista_de_compras) == 0:
+def show_shopping_list():
+    if len(shopping_list) == 0:
         print("Lista de compras vazia...")
     else:
         print("Lista de compras:")
-        for índice, item in enumerate(lista_de_compras):
-            print(f"{índice + 1} - {item}")
+        for i, item in enumerate(shopping_list):
+            print(f"{i + 1} - {item}")
 
 
-def adicionar_item():
+def add_item():
     while True:
         item = input("Qual item deseja adicionar ('sair' para sair) ? ")
         item = item.lower()
         if item == "sair":
             return
         else:
-            lista_de_compras.append(item)
+            shopping_list.append(item)
             print(f"O item '{item}' foi adicionado na lista de compras!\n")
 
 
-def remover_item():
+def remove_item():
     while True:
-        mostrar_lista_de_compras()
+        show_shopping_list()
         item = input("Qual item deseja remover ('sair' para sair) ? ")
         item = item.lower()
         if item == "sair":
             break
-        elif item in lista_de_compras:
-            lista_de_compras.remove(item)
+        elif item in shopping_list:
+            shopping_list.remove(item)
             print(f"O item '{item}' foi removido da lista de compras!\n")
         else:
-            print(MENSAGEM_DE_ERRO)
+            print(ERROR_MESSAGE)
 
 
-def programa_lista_de_compras():
+def main():
     while True:
         system("cls")
-        escolha = tela_inicial()
+        user_option = get_option()
         system("cls")
-        if escolha == 1:
-            adicionar_item()
-        elif escolha == 2:
-            remover_item()
-        elif escolha == 3:
-            mostrar_lista_de_compras()
+        if user_option == 1:
+            add_item()
+        elif user_option == 2:
+            remove_item()
+        elif user_option == 3:
+            show_shopping_list()
             while True:
-                voltar = input("Digite '1' para voltar: ")
-                if voltar.lower() == "1":
-                    return
+                exit = input("Digite '1' para voltar: ")
+                if exit.lower() == "1":
+                    break
                 else:
-                    print(MENSAGEM_DE_ERRO)
+                    print(ERROR_MESSAGE)
         else:
             print("Fechando. Até mais!")
             return
 
 
-lista_de_compras = []
-programa_lista_de_compras()
+shopping_list = []
+main()
