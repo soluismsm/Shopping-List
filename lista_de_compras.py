6f54e01cad6fcdaa81cb7f334bbd5a3492ac2b54
Luis Miguel <vluismw@gmail.com>
2023-03-18 11:20:20 -0300

    Updated Code

--- lista_de_compras.py
+++ lista_de_compras.py
@@ -1,12 +1,10 @@
+import os
+import subprocess
+
 ERROR_MESSAGE = "Opção inválida. tente novamente."
 
 
 def get_option():
-    print("Selecione uma opção")
-    print("1 - Inserir item na lista")
-    print("2 - Apagar tem da lista")
-    print("3 - Ver lista de compras")
-    print("4 - Sair")
     while True:
         try:
             choice = int(input("Escolha (1|2|3|4): "))
@@ -16,7 +14,7 @@ def get_option():
             print(ERROR_MESSAGE)
 
 
-def show_shopping_list():
+def show_shopping_list(shopping_list):
     if len(shopping_list) == 0:
         print("Lista de compras vazia...")
     else:
@@ -25,7 +23,7 @@ def show_shopping_list():
             print(f"{i + 1} - {item}")
 
 
-def add_item():
+def add_item(shopping_list):
     while True:
         item = input("Qual item deseja adicionar ('sair' para sair) ? ")
         item = item.lower()
@@ -36,9 +34,9 @@ def add_item():
             print(f"O item '{item}' foi adicionado na lista de compras!\n")
 
 
-def remove_item():
+def remove_item(shopping_list):
     while True:
-        show_shopping_list()
+        show_shopping_list(shopping_list)
         item = input("Qual item deseja remover ('sair' para sair) ? ")
         item = item.lower()
         if item == "sair":
@@ -50,17 +48,40 @@ def remove_item():
             print(ERROR_MESSAGE)
 
 
+def apagar_console():
+    """
+    Limpa o console do sistema operacional atual.
+    """
+    sistema = os.name
+    if sistema == "nt":
+        subprocess.call("cls", shell=True)
+    else:
+        subprocess.call("clear", shell=True)
+
+
+def show_options():
+    print(
+        """=-=-=-=LISTA DE COMPRAS=-=-=-=
+1 - Inserir um item na Lista.
+2 - Remover item da Lista
+3 - Ver lista de compras.
+4 - Sair
+"""
+    )
+
+
 def main():
+    shopping_list = []
     while True:
-        print("\n" * 100)
+        show_options()
         user_option = get_option()
-        print("\n" * 100)
+        apagar_console()
         if user_option == 1:
-            add_item()
+            add_item(shopping_list)
         elif user_option == 2:
-            remove_item()
+            remove_item(shopping_list)
         elif user_option == 3:
-            show_shopping_list()
+            show_shopping_list(shopping_list)
             while True:
                 exit = input("Digite '1' para voltar: ")
                 if exit.lower() == "1":
@@ -70,7 +91,8 @@ def main():
         else:
             print("Fechando. Até mais!")
             return
+        apagar_console()
 
 
-shopping_list = []
-main()
+if __name__ == "__main__":
+    main()
